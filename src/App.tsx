21b1e0f800b1fe6f1b9f8277f8b6bb46c2ded0b2
Felipe <contatofelipedev@outlook.com>
2025-07-18 18:41:46 -0300

    feat(dependência): adicionando o react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,15 @@
-import { Button, Card, Flex, Space, Typography } from "antd";
-import Cabecalho from "./component/Cabecalho";
-
-const { Text } = Typography;
+import { Route, Routes } from "react-router";
+import Home from "./page/Home";
+import CFuncionario from "./page/CFuncionario";
+import CVeiculo from "./page/CVeiculo";
 
 function App() {
   return (
-    <div
-      style={{
-        margin: 0,
-        padding: 0,
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <Cabecalho />
-
-      <Flex
-        style={{
-          justifyContent: "space-between",
-          alignItems: "center",
-          width: "80%",
-          height: "100%",
-        }}
-      >
-        <Card title="Funcionário" variant="borderless" style={{ width: 300 }}>
-          <Space direction="horizontal">
-            <Button>Cadastrar</Button>
-            <Button>Tabela</Button>
-          </Space>
-        </Card>
-
-        <Card title="Veículo" variant="borderless" style={{ width: 300 }}>
-          <Space direction="horizontal">
-            <Button>Cadastrar</Button>
-            <Button>Tabela</Button>
-          </Space>
-        </Card>
-
-        <Card
-          title="Registro de Viagem"
-          variant="borderless"
-          style={{ width: 300 }}
-        >
-          <Space direction="horizontal">
-            <Button>Cadastrar</Button>
-            <Button>Tabela</Button>
-          </Space>
-        </Card>
-      </Flex>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/funcionario" element={<CFuncionario />} />
+      <Route path="/veiculo" element={<CVeiculo />} />
+    </Routes>
   );
 }
 
