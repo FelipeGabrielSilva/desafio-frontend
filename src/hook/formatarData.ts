5108c2f796b119f89021907a3506dc1ce34ce341
Felipe <contatofelipedev@outlook.com>
2025-07-20 16:40:07 -0300

    feat(hook): conexão com backend e formatação de datas

---
+++ src/hook/formatarData.ts
@@ -0,0 +1,8 @@
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
+
+export const formatarData = (data: Date) => {
+  let formatada = format(data, "MMMM do, yyyy H:mma", { locale: ptBR });
+
+  return formatada;
+};
